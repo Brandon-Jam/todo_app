b2b93cbb3f76375230b906448d4aeb66d331cfe4
Brandon <brandon.jam.dev@gmail.com>
2025-06-19 02:47:26 +0200

    Visuelle Dark Mode

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        if ($user) {
+            return $this->redirectToRoute('app_task_index');
+        }
         return $this->render('home/index.html.twig');
     }
 }
